263010ba7cec17607479d38c1fa701a90f7ddd04
Swati suman <swatisuman@Swatis-MacBook-Pro.local>
2020-04-08 17:52:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello Swati!!! Please see is it refreshing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
